511d0ba7fb94610769c534c8d1eb545747077be9
S-via <168160925+S-via@users.noreply.github.com>
2025-03-09 03:24:17 -0700

    weather_app/setting.py new Skey

--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -13,6 +13,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
+
+#  accessOF API key 
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z2squb0&(_qn3hr@zc=oqlg_yxca1(&&&ie(64^)dpg_ynq60^"
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
