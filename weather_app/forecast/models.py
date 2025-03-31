5b3cbfae6d3e73837267a77a1f8bc298776f5795
S-via <168160925+S-via@users.noreply.github.com>
2025-03-30 17:20:34 -0700

    creates fist model to the usercities

--- weather_app/forecast/models.py
+++ weather_app/forecast/models.py
@@ -3,3 +3,8 @@
 from django.db import models
 
 # Create your models here.
+
+class UserCities(models.Model):
+    city_name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.city_name
